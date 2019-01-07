440d74e6ca464157ceb76cbb806f47a4db9533ad
Hua Xun <huaxun1988@126.com>
2019-01-07 23:15:39 +0800

    新闻列表单张图片Banner处理

--- module_news/src/main/java/com/watson/pureenjoy/news/mvp/ui/adapter/NewsListAdapter.java
+++ module_news/src/main/java/com/watson/pureenjoy/news/mvp/ui/adapter/NewsListAdapter.java
@@ -3,6 +3,8 @@ package com.watson.pureenjoy.news.mvp.ui.adapter;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.bumptech.glide.Glide;
@@ -13,7 +15,6 @@ import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
-import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.watson.pureenjoy.news.R;
 import com.watson.pureenjoy.news.app.NewsConstants;
 import com.watson.pureenjoy.news.http.entity.NewsItem;
@@ -23,6 +24,8 @@ import java.util.List;
 import me.jessyan.armscomponent.commonsdk.core.RouterHub;
 import me.jessyan.armscomponent.commonsdk.utils.StringUtil;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 import static com.watson.pureenjoy.news.app.NewsConstants.PHOTO_SET_ID;
 
 public class NewsListAdapter extends BaseMultiItemQuickAdapter<NewsItem, BaseViewHolder> {
@@ -102,37 +105,41 @@ public class NewsListAdapter extends BaseMultiItemQuickAdapter<NewsItem, BaseVie
     private SliderLayout mSlider;
     private void convertBannerContent(BaseViewHolder helper, NewsItem item) {
         SliderLayout mSlider = helper.getView(R.id.slider);
+        RelativeLayout singleLayout = helper.getView(R.id.single_layout);
         this.mSlider = mSlider;
-        mSlider.removeAllSliders(); //remove
-        for(NewsItem.AdsEntity entity : item.getAds()) {
-            TextSliderView textSliderView = new TextSliderView(context);
-            // initialize a SliderLayout
-            textSliderView
-                    .description(entity.getTitle())
-                    .image(entity.getImgsrc())
-                    .setScaleType(BaseSliderView.ScaleType.Fit)
-                    .setOnSliderClickListener(slider -> ARouter.getInstance().build(RouterHub.NEWS_PHOTO_SET_ACTIVITY)
-                            .withString(PHOTO_SET_ID, (String)slider.getBundle().get(PHOTO_SET_ID))
-                            .navigation());
-            //add your extra information
-            textSliderView.bundle(new Bundle());
-            textSliderView.getBundle().putString(PHOTO_SET_ID,entity.getSkipID());
-            mSlider.addSlider(textSliderView);
+        if (item.getAds().size() > 1) {
+            mSlider.setVisibility(VISIBLE);
+            singleLayout.setVisibility(GONE);
+            mSlider.removeAllSliders(); //remove
+            for(NewsItem.AdsEntity entity : item.getAds()) {
+                TextSliderView textSliderView = new TextSliderView(context);
+                // initialize a SliderLayout
+                textSliderView
+                        .description(entity.getTitle())
+                        .image(entity.getImgsrc())
+                        .setScaleType(BaseSliderView.ScaleType.Fit)
+                        .setOnSliderClickListener(slider -> ARouter.getInstance().build(RouterHub.NEWS_PHOTO_SET_ACTIVITY)
+                                .withString(PHOTO_SET_ID, (String)slider.getBundle().get(PHOTO_SET_ID))
+                                .navigation());
+                //add your extra information
+                textSliderView.bundle(new Bundle());
+                textSliderView.getBundle().putString(PHOTO_SET_ID,entity.getSkipID());
+                mSlider.addSlider(textSliderView);
+            }
+            mSlider.setPresetTransformer(SliderLayout.Transformer.Accordion); //设置过渡动画
+            mSlider.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom); //Indicator位置
+            mSlider.setCustomAnimation(new DescriptionAnimation()); //底部文字展现的动画
+            mSlider.setDuration(5000);
+        } else if (item.getAds().size() == 1){
+            mSlider.setVisibility(GONE);
+            singleLayout.setVisibility(VISIBLE);
+            NewsItem.AdsEntity entity = item.getAds().get(0);
+            ((TextView)helper.getView(R.id.title)).setText(entity.getTitle());
+            Glide.with(context).load(entity.getImgsrc()).into((ImageView) helper.getView(R.id.img));
+            singleLayout.setOnClickListener(v -> ARouter.getInstance().build(RouterHub.NEWS_PHOTO_SET_ACTIVITY)
+                    .withString(PHOTO_SET_ID, entity.getSkipID())
+                    .navigation());
         }
-        mSlider.setPresetTransformer(SliderLayout.Transformer.Accordion); //设置过渡动画
-        mSlider.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom); //Indicator位置
-        mSlider.setCustomAnimation(new DescriptionAnimation()); //底部文字展现的动画
-        mSlider.setDuration(item.getAds().size()>1 ? 5000 : 99999);
-        mSlider.addOnPageChangeListener(new ViewPagerEx.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-
-            @Override
-            public void onPageSelected(int position) {}
-
-            @Override
-            public void onPageScrollStateChanged(int state) {}
-        });
     }
 
 
